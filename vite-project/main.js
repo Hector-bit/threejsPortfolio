1bded62fef0815a7263cf96670139605001cf79e
Hector-bit <hectormartinez.2978@gmail.com>
2022-04-05 21:56:29 -0700

    lights pink and green

--- vite-project/main.js
+++ vite-project/main.js
@@ -3,8 +3,10 @@ import * as THREE from 'three';
 import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import * as dat from 'dat.gui';
-import { Material, QuadraticBezierCurve } from 'three';
 // import * as myChair from './models/scene.gltf'; 
+import { RectAreaLightUniformsLib } from './extra/RectAreaLightUniformLib';
+
+RectAreaLightUniformsLib.init();
 
 // DEBUG
 const gui = new dat.GUI();
@@ -12,13 +14,15 @@ var settings = {
   playhead: 0.001,
   blockX: 0.5,
   blockY: 16,
-  blockZ: 2
+  blockZ: 2,
+  alight: 0.5,
 }
 
 gui.add(settings, "playhead", 0.001, 1, 0.001);
 gui.add(settings, "blockX", -50, 50, 1)
 gui.add(settings, "blockY", -50, 50, 1)
 gui.add(settings, "blockZ", -50, 50, 1)
+gui.add(settings, 'alight', 0.01, 1, 0.01)
 
 // SCENE
 const scene = new THREE.Scene();
@@ -45,16 +49,33 @@ loader.load('models/room.gltf', function(gltf){
   console.log( error );
 });
 
+//Rect light for computer screen youtube vid aat 2:35 https://www.youtube.com/watch?v=T6PhV4Hz0u4
+//if i need help
 
-//scene and lights
-const pointLight = new THREE.PointLight(0xffffff)
+
+// SCENE AND LIGHTS
+var width = 5;
+var height = 5;
+var rectLight = new THREE.RectAreaLight( 0xffffff, 2,  width, height );
+var pinkRectLight = new THREE.RectAreaLight( 0xFFC0CB, 1, 35, 35);
+var greenRectLight = new THREE.RectAreaLight( 0x00FF00, 1, 30, 30);
+rectLight.position.set( -1, 16.75, 2.25);
+pinkRectLight.position.set(-1,  16.75, -35);
+greenRectLight.position.set(-1,  16.75, 45);
+rectLight.lookAt( 30,16.75,2.25 );
+pinkRectLight.lookAt( 0.5, 16, 2);
+greenRectLight.lookAt( 0.5, 16, 2);
+// var rectLightHelper = new THREE.RectAreaLightHelper(rectLight );
+// scene.add( rectLightHelper );
+scene.add( rectLight, pinkRectLight, greenRectLight);
+const pointLight = new THREE.PointLight(0x2c3f47, 1)
 const lightHelper = new THREE.PointLightHelper(pointLight)
 scene.add(lightHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
-pointLight.position.set(30,30,10)
+// const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, );
+pointLight.position.set(25,25,10)
 
 
 //FAKE SCROLL / SCROLL PERCENT
@@ -104,15 +125,6 @@ document.body.onscroll = function () {
 
 
 
-const bezier = new THREE.CubicBezierCurve3(
-  new THREE.Vector3(-0.5, 0.55, 0),
-  new THREE.Vector3(-0.5, 0.1, 0),
-  new THREE.Vector3(-0.45, 0.1, 0),
-  new THREE.Vector3(0, 0.1, 0)
-);
-
-
-
 function playScrollAnimations() {
   animationScripts.forEach((a) => {
     if (scrollPercent >= a.start && scrollPercent < a.end){
@@ -120,32 +132,25 @@ function playScrollAnimations() {
     }
   })
 }
+
 let scrollPercent = 0;
+
 //animations
 function animate() 
 {
   requestAnimationFrame( animate )
-
-  // someObject.rotation.x += 0.001;
-  // someObject.rotation.y += 0.005;
-  // someObject.rotation.z += 0.001;
   playScrollAnimations()
 
-  // controls.update()
   const playhead = settings.playhead;
-  // target.position.x = settings.blockX;
-  // target.position.y = settings.blockY;
-  // target.position.z = settings.blockZ;
+  rectLight.position.x = settings.blockX;
+  rectLight.position.y = settings.blockY;
+  rectLight.position.z = settings.blockZ;
 
   //update target
 // target.position.x = (playhead) - 0.5;
-  // camera.lookAt(target.position);
-  // camera.position.x = settings.blockX + 5;
-  // camera.position.y = settings.blockY;
-  // camera.position.z = settings.blockZ;
-  // const play_pos = bezier.getPoint(playhead);
   // camera.position.set(play_pos.x, play_pos.y, play_pos.z);
-  console.log(target.position.z)
+  // ambientLight = new THREE.AmbientLight(0xffffff , settings.alight);
+  // console.log(target.position.z)
   renderer.render( scene, camera );
 }
 
