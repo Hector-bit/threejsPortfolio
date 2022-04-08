e0b085156ef353563d8d805a47a12e2d95cb48ec
Hector-bit <hectormartinez.2978@gmail.com>
2022-04-07 23:39:30 -0700

    lighting helper

--- vite-project/main.js
+++ vite-project/main.js
@@ -5,6 +5,8 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import * as dat from 'dat.gui';
 // import * as myChair from './models/scene.gltf'; 
 import { RectAreaLightUniformsLib } from './extra/RectAreaLightUniformLib';
+import { seededRandom } from 'three/src/math/MathUtils';
+import { RectAreaLight } from 'three';
 
 RectAreaLightUniformsLib.init();
 
@@ -12,17 +14,40 @@ RectAreaLightUniformsLib.init();
 const gui = new dat.GUI();
 var settings = {
   playhead: 0.001,
-  blockX: 0.5,
-  blockY: 16,
-  blockZ: 2,
-  alight: 0.5,
+  blockX: 40,
+  blockY: 40,
+  blockZ: -22,
+  greenx: 24,
+  greeny: 26,
+  greenz: 24,
+  targetx: 10,
+  targety: 18,
+  targetz: 2.5,
+  windowx: 10,
+  windowy: 10,
+  windowz: -28,
+  windowBright: 1,
+  greenBright: 1,
+  pinkBright: 1
 }
 
-gui.add(settings, "playhead", 0.001, 1, 0.001);
-gui.add(settings, "blockX", -50, 50, 1)
-gui.add(settings, "blockY", -50, 50, 1)
-gui.add(settings, "blockZ", -50, 50, 1)
-gui.add(settings, 'alight', 0.01, 1, 0.01)
+gui.add(settings, "playhead", 0.001, 1, 0.001)
+gui.add(settings, "blockX", -80, 80, 1)
+gui.add(settings, "blockY", -80, 80, 1)
+gui.add(settings, "blockZ", -80, 80, 1)
+gui.add(settings, "greenx", -80, 80, 1)
+gui.add(settings, "greeny", -80, 80, 1)
+gui.add(settings, "greenz", -80, 80, 1)
+gui.add(settings, "targetx", -80, 80, 1)
+gui.add(settings, "targety", -80, 80, 1)
+gui.add(settings, "targetz", -80, 80, 1)
+gui.add(settings, "windowx", -80, 80, 1)
+gui.add(settings, "windowy", -80, 80, 1)
+gui.add(settings, "windowz", -80, 80, 1)
+gui.add(settings, "windowBright", 0, 100, 1)
+gui.add(settings, "greenBright", 0, 100, 1)
+gui.add(settings, "pinkBright", 0, 100, 1)
+
 
 // SCENE
 const scene = new THREE.Scene();
@@ -38,8 +63,6 @@ camera.position.setX(20);
 camera.position.setY(20);
 renderer.render( scene, camera );
 
-// OBJECTS
-
 // ROOM
 const loader = new GLTFLoader();
 
@@ -49,25 +72,35 @@ loader.load('models/room.gltf', function(gltf){
   console.log( error );
 });
 
-//Rect light for computer screen youtube vid aat 2:35 https://www.youtube.com/watch?v=T6PhV4Hz0u4
-//if i need help
-
+// OBJECTS
+const mat = new THREE.MeshStandardMaterial({color: 'red'});
+const geo = new THREE.BoxGeometry(1,1,1);
+const target = new THREE.Mesh(geo, mat);
+const rectLightTarget = new THREE.Mesh(geo, mat);
+target.position.set(-1, 16.75, 2.25);
+camera.position.set(0, 16.75, 2.25)
+scene.add(target, rectLightTarget);
 
 // SCENE AND LIGHTS
-var width = 5;
-var height = 5;
-var rectLight = new THREE.RectAreaLight( 0xffffff, 2,  width, height );
-var pinkRectLight = new THREE.RectAreaLight( 0xFFC0CB, 1, 35, 35);
-var greenRectLight = new THREE.RectAreaLight( 0x00FF00, 1, 30, 30);
+var rectLight = new THREE.RectAreaLight( 0xffffff, 2,  5, 5 );
+var windowLight = new THREE.RectAreaLight( 0xffffff, settings.windowBright, 25, 25);
+var greenRectLight = new THREE.RectAreaLight( 0x00FF00, settings.greenBright, 35, 35);
+var pinkRectLight = new THREE.RectAreaLight( 0xFFC0CB, settings.pinkBright, 30, 30);
+
+// LIGHT HELPERS
+// const helper_windowLight = new RectAreaLightHelper( windowLight );
+// windowLight.add( helper_windowLight );
+const plane = new THREE.Plane( new THREE.Vector3( 10, 10, 20 ), 0 );
+const helper = new THREE.PlaneHelper( plane, 10, 0xffff00 );
+scene.add( helper );
+
 rectLight.position.set( -1, 16.75, 2.25);
-pinkRectLight.position.set(-1,  16.75, -35);
-greenRectLight.position.set(-1,  16.75, 45);
-rectLight.lookAt( 30,16.75,2.25 );
-pinkRectLight.lookAt( 0.5, 16, 2);
-greenRectLight.lookAt( 0.5, 16, 2);
-// var rectLightHelper = new THREE.RectAreaLightHelper(rectLight );
-// scene.add( rectLightHelper );
-scene.add( rectLight, pinkRectLight, greenRectLight);
+rectLight.lookAt( 30, 16.75, 2.25 );
+windowLight.lookAt( 15, 15, 450 );
+
+scene.add( rectLight, pinkRectLight, greenRectLight, windowLight );
+
+
 const pointLight = new THREE.PointLight(0x2c3f47, 1)
 const lightHelper = new THREE.PointLightHelper(pointLight)
 scene.add(lightHelper)
@@ -91,14 +124,7 @@ function scalePercent(start, end){
 
 var animationScripts = [];
 
-const mat = new THREE.MeshStandardMaterial({color: 'red'});
-const geo = new THREE.BoxGeometry(1,1,1);
-const target = new THREE.Mesh(geo, mat);
-target.position.x = -1;
-target.position.y = 16.75;
-target.position.z = 2.25;
-camera.position.set(0, 16.75, 2.25)
-scene.add(target);
+
 
 animationScripts.push({
   start: 0,
@@ -142,15 +168,30 @@ function animate()
   playScrollAnimations()
 
   const playhead = settings.playhead;
-  rectLight.position.x = settings.blockX;
-  rectLight.position.y = settings.blockY;
-  rectLight.position.z = settings.blockZ;
+  pinkRectLight.position.x = settings.blockX;
+  pinkRectLight.position.y = settings.blockY;
+  pinkRectLight.position.z = settings.blockZ;
+  pinkRectLight.lookAt(rectLightTarget.position);
 
-  //update target
-// target.position.x = (playhead) - 0.5;
-  // camera.position.set(play_pos.x, play_pos.y, play_pos.z);
-  // ambientLight = new THREE.AmbientLight(0xffffff , settings.alight);
-  // console.log(target.position.z)
+  greenRectLight.position.x = settings.greenx;
+  greenRectLight.position.y = settings.greeny;
+  greenRectLight.position.z = settings.greenz;
+  greenRectLight.lookAt(rectLightTarget.position);
+
+  rectLightTarget.position.x = settings.targetx;
+  rectLightTarget.position.y = settings.targety;
+  rectLightTarget.position.z = settings.targetz;
+
+  windowLight.position.x = settings.windowx;
+  windowLight.position.y = settings.windowy;
+  windowLight.position.z = settings.windowz;
+
+  // BRIGHTNESS
+  pinkRectLight.intensity = settings.pinkBright; 
+  greenRectLight.intensity = settings.greenBright;
+  windowLight.intensity = settings.windowBright;
+  // windowLight.position.set(-1,  16.75, -35);
+
   renderer.render( scene, camera );
 }
 
